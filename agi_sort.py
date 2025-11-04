0ed57fc45e8387390525742d8047aa466a777fbe
Agilesh Arumugam <agilesh1081@gmail.com>
2025-11-04 10:29:37 +0530

    initial commit
    Implement AGI sort algorithm using bucket sort and heaps.

---
+++ agi_sort.py
@@ -0,0 +1,25 @@
+from typing import List
+import heapq
+import math
+
+def agi_sort(unsorted_array: List[int]) -> List[int]:
+    n = len(unsorted_array)
+    min_num = min(unsorted_array)
+    max_num = max(unsorted_array)
+    buckets = [[] for _ in range(n)]
+
+    for num in unsorted_array:
+        i = math.floor(((num - min_num) / (max_num - min_num)) * (n - 1))
+        heapq.heappush(buckets[i], num)
+    
+    result = []
+    for heap in buckets:
+        while heap:
+            number = heapq.heappop(heap)
+            result.append(number)
+    
+    return result
+
+unsorted_array = [12, 11, 13, 5, 6]
+sorted_array = agi_sort(unsorted_array)
+print(sorted_array)
