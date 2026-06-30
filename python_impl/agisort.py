c3a76a03d562fdf11a6f21007de97436020a8d4f
AgiGames <agilesh1081@gmail.com>
2026-06-30 17:25:27 +0530

    updated python implementation to be consistent

--- python_impl/agisort.py
+++ python_impl/agisort.py
@@ -1,28 +1,32 @@
-from typing import List
+from typing import List, Callable
 import heapq
 import math
 
-def agisort(unsorted_array: List[int]) -> List[int]:
-    n = len(unsorted_array)
-    min_num = min(unsorted_array)
-    max_num = max(unsorted_array)
-    if max_num == min_num:
-        return unsorted_array
+def agisort(input_list: List[int], score: Callable[[object], int]) -> List[int]:
+    n = len(input_list)
+    min_score = min(input_list, key=lambda x: score(x))
+    max_score = max(input_list, key=lambda x: score(x))
+    if min_score == max_score:
+        return input_list
     
-    buckets = [[] for _ in range(n)]
+    heaps = [[] for _ in range(n)]
 
-    for num in unsorted_array:
-        i = math.floor(((num - min_num) / (max_num - min_num)) * (n - 1))
-        heapq.heappush(buckets[i], num)
+    for ith_element in input_list:
+        i = math.floor(((score(ith_element) - min_score) / (max_score - min_score)) * (n - 1))
+        heapq.heappush(heaps[i], ith_element)
     
-    result = []
-    for heap in buckets:
+    i = 0
+    for heap in heaps:
         while heap:
-            number = heapq.heappop(heap)
-            result.append(number)
+            ith_element = heapq.heappop(heap)
+            input_list[i] = ith_element
+            i += 1
     
-    return result
+    return input_list
+
+def score(num: int):
+    return num
 
 unsorted_array = [12, 11, 13, 5, 6]
-sorted_array = agisort(unsorted_array)
+sorted_array = agisort(unsorted_array, score)
 print(sorted_array)
